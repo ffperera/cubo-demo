b0e88514b8109f2c03f595e747646bded2a1dc42
Felipe Fernandez Perera <felipe@epsilon-eridani.com>
2025-05-04 15:10:59 +0200

    enhance index.php for better error handling and Latte template support

--- root/index.php
+++ root/index.php
@@ -1,5 +1,6 @@
 <?php
 
+// front controller / access point
 
 require __DIR__ . '/../vendor/autoload.php';
 include_once __DIR__ . '/../src/config/routing.php';
@@ -14,19 +15,25 @@ $dotenv->load(__DIR__ . '/../.env');
 
 use FFPerera\Cubo\Controller;
 use FFPerera\Cubo\Render;
+use FFPerera\Cubo\Response;
 
 $logger = new \Monolog\Logger('app');
 // $logger->pushHandler(new \Monolog\Handler\StreamHandler(__DIR__ . '/logs/app.log', \Monolog\Level::Debug));
 $logger->pushHandler(new \Monolog\Handler\StreamHandler("php://stdout", \Monolog\Level::Debug));
 
 
-$controller = new Controller($routes, $logger);
 
 try {
+
+    $controller = new Controller($routes, $logger, true);
+
     $view = $controller->run();
 
     if ($view && $view instanceof \FFPerera\Cubo\View) {
 
+        // check if it is a template for latte
+        // this is a hack, because this is a sample project
+        // a real project probably will work with only one template engine
         if ($view->isset('templatte')) {
             $render = new \FFPerera\Lib\LatteRender($view, dirname($srcDir) . '/latte', true);
         } else {
@@ -34,6 +41,18 @@ try {
         }
 
         $render->send();
+    }
+} catch (InvalidArgumentException $e) {
+
+    if (str_contains($e->getMessage(), 'No route found')) {
+        // Handle 404 error
+        (new Response('404 Not Found', [
+            'Content-Type' => 'text/html; charset=utf-8',
+            'X-Powered-By' => 'Cubo',
+            'statusCode' => 404,
+            'statusText' => 'Not Found',
+            'protocolVersion' => '1.1',
+        ]))->send();
     }
 } catch (Exception $e) {
     // TODO: catch every posible exception
